504760728c4ba1af385f1f021abe4faab234dca5
Denson Oracion <miguel.oracion@outlook.com>
2024-10-23 14:51:02 +0400

    Coding activities

---
+++ # Python If-else statements.py
@@ -0,0 +1,48 @@
+# Python If-else statements
+
+# 1. Basic 'if-else' 
+age = int(input("Enter your age:"))
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are not an adult.")
+
+# 2. "If-elif-else" Statement
+marks = int(input("Enter your marks:"))
+
+if marks >= 90:
+    print("Grade: A")
+elif marks >= 75:
+    print("Grade: B")
+elif marks >= 60:
+    print("Grade: C")
+else:
+    print("Grade: F")
+
+# 3. Multiple conditions with "and" and "or"
+num = int(input("Enter a number:"))
+
+if num > 0 and num < 10:
+    print("The number is a single-digit positive number.")
+else:
+    print("The number is either negative or has more than one digit.")
+
+# 4. Even or Odd
+number = int(input("Enter a number:"))
+
+if number % 2 == 0 
+    print(f"{number} is an even number." )
+else:
+    print(f"{number} is an odd number.")
+
+# 5. Nested "if-else"
+digit = int(input("Enter a number:"))
+
+if digit >= 0 
+    if number == 0
+        print("The number is zero.")
+    else:
+        print("The number is positive.")
+else:
+    print("The number is negative.")
---
+++ SimpleMathOperations.py
@@ -0,0 +1,24 @@
+#Listing our variables
+#1. Addition
+a = 20
+b = 10
+sum = a + b
+print("The sum is", sum)
+
+#2. Subtraction
+a = 20
+b = 10 
+difference = a - b 
+print("The difference is", difference)
+
+#3.  Multiplication
+a = 20
+b = 10
+product = a * b
+print("The product is", product)
+
+#4. Division
+a = 20
+b = 10
+quotient = a / b 
+print("The quotient is", quotient)
